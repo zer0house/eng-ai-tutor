c1816f8638b827414bd8ff96fde4177b4b7e721c
zer0house <102198136+zer0house@users.noreply.github.com>
2023-11-20 13:09:05 +0900

    구글 폰트 적용

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -25,6 +25,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Nunito+Sans&display=swap"
             rel="stylesheet"
           />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <script dangerouslySetInnerHTML={{ __html: setInitialTheme }} />
